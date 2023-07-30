cef7e4a4d63eb8cea8ba7293fe01197211bb32c4
Harry Rutter <harrygrutter@gmail.com>
2023-07-30 16:14:37 +0100

    Tidy up controllers

---
+++ StockAPI/Controllers/StockTradeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockAPI.Models;
+
+namespace StockAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StockTradeController : ControllerBase
+{
+    private readonly ILogger<StockTradeController> _logger;
+
+    public StockTradeController(ILogger<StockTradeController> logger)
+    {
+        _logger = logger;
+    }
+}
+
